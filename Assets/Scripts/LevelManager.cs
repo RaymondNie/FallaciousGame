f61e9bd90b68cda4e6a8d6e9ea95e6e92ba01678
Raymond Nie <nieraymond@gmail.com>
2017-12-08 17:50:06 -0500

    Designed overall game structure and created sample scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
-
+	
+	// Not sure if we going to use this since scenes should just be randomly loaded
 	public void load_level(string name){
 		Debug.Log("Level load requested for " + name);
 		Application.LoadLevel(name);
 	}
-
+	
 }
