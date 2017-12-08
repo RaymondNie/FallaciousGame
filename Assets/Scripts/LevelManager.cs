de0b2876fb99af3078362f0d20276d0b92915d98
Raymond Nie <nieraymond@gmail.com>
2017-12-08 17:01:36 -0500

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void load_level(string name){
+		Debug.Log("Level load requested for " + name);
+		Application.LoadLevel(name);
+	}
+
+}
