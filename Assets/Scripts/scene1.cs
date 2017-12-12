b72baa27ed8c2454d1b589ff1e0e4916d4093888
Raymond Nie <nieraymond@gmail.com>
2017-12-12 16:45:40 -0500

    Text rendering finished

--- Assets/Scripts/scene1.cs
+++ Assets/Scripts/scene1.cs
@@ -11,20 +11,26 @@ public class scene1 : MonoBehaviour {
 	public Text speaker;
 	public Text option1;
 	public Text option2;
+	public GameObject button1;
+	public GameObject button2;
 
 	// Printing text
 	private IEnumerator scrollingTextRoutine;
 	private string currText;
 	float scrollSpeed = 0.0125f;
 
+	// Different states of the scene
 	enum State{Start, Agree, Disagree, End};
-
 	State currState;
+
+	// List of possible levels
+	int[] validLevelNumbers = new int [2] {0,2};
 
 	void Start(){
 		// Initialize start scene
 		currState = State.Start;
 		scrollingTextRoutine = scrollingText ();
+		// Starting text
 		currText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut aliquam ligula, sed tempor magna. Aenean hendrerit justo eget neque blandit rutrum. Quisque in augue nisi. Maecenas quis lobortis sapien. Integer lobortis felis ut mi finibus, sit amet pharetra elit mollis. Sed aliquam turpis viverra mauris sodales tincidunt in vitae lectus. Proin vitae dolor rutrum, convallis lacus non, tincidunt nibh. Nulla facilisi. Fusce viverra tempus elit, id pulvinar lectus interdum at. Sed turpis nulla, mattis ac bibendum eu, sodales id justo. Morbi egestas quis orci a placerat. Fusce euismod porta tincidunt. Praesent ac quam id diam cursus sollicitudin. Vivamus ac orci mi. Etiam vitae magna a nibh molestie ultrices et vel sapien. Pellentesque sed finibus sapien.\n\n";
 		StartCoroutine (scrollingTextRoutine);
 	}
@@ -34,12 +40,13 @@ public class scene1 : MonoBehaviour {
 			StopCoroutine (scrollingTextRoutine);
 			scrollingTextRoutine = scrollingText ();
 			text.text = currText;
+			button1.SetActive (true);
+			button2.SetActive (true);
 		}
 	}
 
 	public void changeState(bool option){
 		// Somewhere we keep track of current state and set transitions..
-
 		// If statements...
 		if (currState == State.Start) {
 			if (option) {
@@ -55,10 +62,14 @@ public class scene1 : MonoBehaviour {
 	}
 
 	IEnumerator scrollingText(){
+		button1.SetActive (false);
+		button2.SetActive (false);
 		for (int i = 0; i <= currText.Length; ++i) {
 			text.text = currText.Substring (0, i);
 			yield return new WaitForSeconds(scrollSpeed);
 		}
+		button1.SetActive (true);
+		button2.SetActive (true);
 	}
 
 	void stateAgree(){
@@ -66,7 +77,7 @@ public class scene1 : MonoBehaviour {
 		currState = State.Agree;
 		speaker.text = "Raymond";
 		option1.text = "End game";
-		currText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut aliquam ligula, sed tempor magna. Aenean hendrerit justo eget neque blandit rutrum. Quisque in augue nisi. Maecenas quis lobortis sapien. Integer lobortis felis ut mi finibus, sit amet pharetra elit mollis. Sed aliquam turpis viverra mauris sodales tincidunt in vitae lectus. Proin vitae dolor rutrum, convallis lacus non, tincidunt nibh. Nulla facilisi. Fusce viverra tempus elit, id pulvinar lectus interdum at. Sed turpis nulla, mattis ac bibendum eu, sodales id justo. Morbi egestas quis orci a placerat. Fusce euismod porta tincidunt. Praesent ac quam id diam cursus sollicitudin. Vivamus ac orci mi. Etiam vitae magna a nibh molestie ultrices et vel sapien. Pellentesque sed finibus sapien.\n\n";
+		currText = "Raymond";
 		StartCoroutine (scrollingTextRoutine);
 	}
 
@@ -81,7 +92,8 @@ public class scene1 : MonoBehaviour {
 	void stateEnd(){
 		currState = State.End;
 		// Load next scene...
-		SceneManager.LoadScene("GameOver");
+		int randomIndex = Random.Range(0, validLevelNumbers.Length);
+		Application.LoadLevel(validLevelNumbers[randomIndex]);
 	}
 
 
